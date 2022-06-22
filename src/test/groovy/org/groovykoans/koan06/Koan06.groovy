02f50378d184da465fbbf7c02f0fa55eb8c01bc9
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-06-22 13:44:27 +0300

    passed Koan06-1

--- src/test/groovy/org/groovykoans/koan06/Koan06.groovy
+++ src/test/groovy/org/groovykoans/koan06/Koan06.groovy
@@ -34,8 +34,14 @@ class Koan06 extends GroovyTestCase {
         // Groovy-fy the above code, using StringBuilder and with() to get the same result in Groovy
         String groovyResult
         // ------------ START EDITING HERE ----------------------
+        groovyResult = new StringBuilder().with {
+            append("roses are #FF0000\\n")
+            append("violets are #0000FF\\n")
+            append("all my base\\n")
+            append("are belong to you\\n")
 
-
+            return it
+        }
         // ------------ STOP EDITING HERE  ----------------------
         assert groovyResult == javaResult
     }

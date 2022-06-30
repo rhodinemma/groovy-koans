43a4f75b35dc09c7ca2b8ba8b7bc8407a8d582b9
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-06-30 10:13:19 +0300

    passed Koan12-02

--- src/test/groovy/org/groovykoans/koan12/Koan12.groovy
+++ src/test/groovy/org/groovykoans/koan12/Koan12.groovy
@@ -46,8 +46,9 @@ class Koan12 extends GroovyTestCase {
         Factory factory = new Factory()
         long startTime = System.currentTimeMillis()
         // ------------ START EDITING HERE ----------------------
-
-
+        def stub = new Expando();
+        stub.work = { number -> number + 10 }
+        factory.worker = stub
         // ------------ STOP EDITING HERE  ----------------------
         factory.work()
         long endTime = System.currentTimeMillis()
@@ -59,8 +60,8 @@ class Koan12 extends GroovyTestCase {
         // Try to experiment with Map Coercion too - http://docs.groovy-lang.org/latest/html/documentation/index.html#_mocking_and_stubbing
         startTime = System.currentTimeMillis()
         // ------------ START EDITING HERE ----------------------
-
-
+        Worker worker = { number -> number + 10 } as Worker
+        StaticFactory.metaClass.static.getWorker = { worker }
         // ------------ STOP EDITING HERE  ----------------------
        new StaticFactory(numbers: 1..10).work()
         endTime = System.currentTimeMillis()
